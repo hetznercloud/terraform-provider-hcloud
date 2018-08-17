e7813233950884abf48ff46a6108de6febec390d
Nico Schieder <nico.schieder@hetzner-cloud.de>
2018-08-17 10:01:31 +0200

    Cleanup tests

--- hcloud/data_source_hcloud_floatingip_test.go
+++ hcloud/data_source_hcloud_floatingip_test.go
@@ -1,7 +1,6 @@
 package hcloud
 
 import (
-	"context"
 	"fmt"
 	"testing"
 
@@ -19,51 +18,35 @@ func init() {
 var floatingIPForDataSource *hcloud.FloatingIP
 
 func TestAccHcloudDataSourceFloatingIP(t *testing.T) {
-	floatingIPForDataSource, _ = createTestFloatingIP()
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccHcloudPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccHcloudCheckFloatingIPDataSourceConfig(floatingIPForDataSource),
+				Config: testAccHcloudCheckFloatingIPDataSourceConfig(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccHcloudCheckFloatingIPExists("data.hcloud_floating_ip.ip_1", floatingIPForDataSource),
 					resource.TestCheckResourceAttr(
 						"data.hcloud_floating_ip.ip_1", "type", "ipv4"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_floating_ip.ip_1", "home_location", "fsn1"),
 				),
 			},
 		},
 	})
 
 	testDataSourceCleanup()
-
 }
 
-func createTestFloatingIP() (*hcloud.FloatingIP, error) {
-	client, _ := createClient()
-	ctx := context.Background()
-	description := "my-floating-ip.com"
-
-	opts := hcloud.FloatingIPCreateOpts{
-		Type:         hcloud.FloatingIPType(hcloud.FloatingIPTypeIPv4),
-		Description:  hcloud.String(description),
-		HomeLocation: &hcloud.Location{Name: "hel1"},
-	}
-
-	response, _, err := client.FloatingIP.Create(ctx, opts)
-	if err != nil {
-		return nil, err
-	}
-
-	return response.FloatingIP, nil
-}
-
-func testAccHcloudCheckFloatingIPDataSourceConfig(ip *hcloud.FloatingIP) string {
+func testAccHcloudCheckFloatingIPDataSourceConfig() string {
 	return fmt.Sprintf(`
+resource "hcloud_floating_ip" "floating_ip" {
+  type      = "ipv4"
+  home_location = "fsn1"
+}
 data "hcloud_floating_ip" "ip_1" {
-  ip_address = "%s"
-}`, ip.IP)
+  ip_address = "${hcloud_floating_ip.floating_ip.ip_address}"
+}`)
 }
 
 func testDataSourceCleanup() {
