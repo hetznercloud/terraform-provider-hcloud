690067d320b1e8506ab6008a2a64b9ed7084cede
Konstantin Chugalinskiy <kchugalinskiy@users.noreply.github.com>
2018-12-05 11:40:32 +0300

    Add `hcloud_datacenter(s)` resources (#55)
    * Add datacenter data source
    
    * Fix documentation navigation
    
    * Don't count check the datacenter count

---
+++ hcloud/data_source_hcloud_datacenter_test.go
@@ -0,0 +1,50 @@
+package hcloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func init() {
+	resource.AddTestSweepers("data_source_datacenter", &resource.Sweeper{
+		Name: "hcloud_datacenter_data_source",
+	})
+}
+func TestAccHcloudDataSourceDatasource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccHcloudPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccHcloudCheckDatacenterDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_1", "id", "1"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_1", "name", "fsn1-dc8"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_1", "description", "Falkenstein 1 DC 8"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_2", "id", "4"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_2", "name", "fsn1-dc14"),
+					resource.TestCheckResourceAttr(
+						"data.hcloud_datacenter.ds_2", "description", "Falkenstein 1 DC14"),
+				),
+			},
+		},
+	})
+}
+
+func testAccHcloudCheckDatacenterDataSourceConfig() string {
+	return fmt.Sprintf(`
+data "hcloud_datacenter" "ds_1" {
+  name = "fsn1-dc8"
+}
+data "hcloud_datacenter" "ds_2" {
+  id = 4
+}
+`)
+}
