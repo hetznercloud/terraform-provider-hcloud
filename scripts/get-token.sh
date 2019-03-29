0d7a7b40d7fc8b11b4f3d62d85a5dae85f53637e
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2019-03-29 07:47:20 +0100

    typo

--- scripts/get-token.sh
+++ scripts/get-token.sh
@@ -2,12 +2,10 @@
 
 set -e
 resp=$(curl -A 'travis-terraform-provider' --header 'Authorization: Bearer '"$TTS_TOKEN"'' -X POST https://tt-service.hetzner.cloud/token -o resp.json)
-if grep -q Unauthorized "resp.json"
+if grep -q token "resp.json"
 then
-    cat resp.json
-    exit 1
-
-else
     token=$(cat resp.json | jq -r '.token')
     echo $token
+else
+    exit 1
 fi
