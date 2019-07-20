be801c27aac6ddd3a1171a643520addc48f857e6
Craig Furman <craig.furman89@gmail.com>
2019-07-20 14:09:13 +0100

    Add hcloud_ssh_keys datasource
    Finds multiple ssh keys by label selector. If no selector specified,
    will retrieve all keys from the project.

---
+++ hcloud/data_source_hcloud_sshkeys.go
@@ -0,0 +1,84 @@
+package hcloud
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hetznercloud/hcloud-go/hcloud"
+)
+
+func dataSourceHcloudSSHKeys() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceHcloudSSHKeysRead,
+		Schema: map[string]*schema.Schema{
+			"with_selector": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ssh_keys": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"fingerprint": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"public_key": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"labels": {
+							Type:     schema.TypeMap,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+func dataSourceHcloudSSHKeysRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*hcloud.Client)
+
+	labelSelector := d.Get("with_selector")
+	labelSelectorStr, _ := labelSelector.(string)
+
+	opts := hcloud.SSHKeyListOpts{
+		ListOpts: hcloud.ListOpts{
+			LabelSelector: labelSelectorStr,
+		},
+	}
+	keys, err := client.SSHKey.AllWithOpts(context.Background(), opts)
+	if err != nil {
+		return err
+	}
+	var keyMaps []map[string]interface{}
+	id := ""
+	for _, key := range keys {
+		if id != "" {
+			id += "-"
+		}
+		id += fmt.Sprintf("%d", key.ID)
+		keyMaps = append(keyMaps, map[string]interface{}{
+			"id":          key.ID,
+			"name":        key.Name,
+			"fingerprint": key.Fingerprint,
+			"public_key":  key.PublicKey,
+			"labels":      key.Labels,
+		})
+	}
+
+	d.SetId(id)
+	d.Set("ssh_keys", keyMaps)
+	return nil
+}
