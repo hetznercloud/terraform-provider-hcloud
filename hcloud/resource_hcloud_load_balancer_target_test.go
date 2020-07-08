122665780396935c091d5f83d609723b4f66c4a2
Ferdinand Hofherr <ferdinand.hofherr@hetzner-cloud.de>
2020-07-08 11:41:08 +0200

    Add depends_on clause
    This ensures that the Load Balancer as well as the Server are attached
    to the same network before creating the Load Balancer server target.

--- hcloud/resource_hcloud_load_balancer_target_test.go
+++ hcloud/resource_hcloud_load_balancer_target_test.go
@@ -118,7 +118,10 @@ resource "hcloud_load_balancer_target" "lb_test_target" {
 	server_id        = "${hcloud_server.lb_server_target.id}"
 	use_private_ip   = true
 
-	depends_on = [ hcloud_load_balancer_network.target_test_lb_network ]
+	depends_on = [
+		hcloud_load_balancer_network.target_test_lb_network,
+		hcloud_server_network.lb_server_network
+	]
 }
 	`, rInt, rInt, rInt)
 }
