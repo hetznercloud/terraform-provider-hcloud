2ca681f15cb6128c236a7f8c5a9913d4c0c35a61
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-08-12 12:45:07 +0200

    Refactor hcloud_image datasource tests

---
+++ internal/image/data_source_test.go
@@ -0,0 +1,51 @@
+package image_test
+
+import (
+	"github.com/terraform-providers/terraform-provider-hcloud/internal/image"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/terraform-providers/terraform-provider-hcloud/internal/loadbalancer"
+	"github.com/terraform-providers/terraform-provider-hcloud/internal/testsupport"
+	"github.com/terraform-providers/terraform-provider-hcloud/internal/testtemplate"
+)
+
+const TestImageName = "ubuntu-20.04"
+const TestImageId = "15512617"
+
+func TestAccHcloudDataSourceImageTest(t *testing.T) {
+	tmplMan := testtemplate.Manager{}
+
+	imageByName := &image.DData{
+		ImageName: TestImageName,
+	}
+	imageByName.SetRName("image_by_name")
+	imageByID := &image.DData{
+		ImageID: TestImageId,
+	}
+	imageByID.SetRName("image_by_id")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     testsupport.AccTestPreCheck(t),
+		Providers:    testsupport.AccTestProviders(),
+		CheckDestroy: testsupport.CheckResourcesDestroyed(loadbalancer.ResourceType, loadbalancer.ByID(t, nil)),
+		Steps: []resource.TestStep{
+			{
+				Config: tmplMan.Render(t,
+					"testdata/d/hcloud_image", imageByName,
+					"testdata/d/hcloud_image", imageByID,
+				),
+
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(imageByName.TFID(),
+						"name", TestImageName),
+					resource.TestCheckResourceAttr(imageByName.TFID(), "id", TestImageId),
+
+					resource.TestCheckResourceAttr(imageByID.TFID(),
+						"name", TestImageName),
+					resource.TestCheckResourceAttr(imageByID.TFID(), "id", TestImageId),
+				),
+			},
+		},
+	})
+}
---
+++ internal/image/testing.go
@@ -0,0 +1,21 @@
+package image
+
+import (
+	"fmt"
+	"github.com/terraform-providers/terraform-provider-hcloud/internal/testtemplate"
+)
+
+// DData defines the fields for the "testdata/d/hcloud_image"
+// template.
+type DData struct {
+	testtemplate.DataCommon
+
+	ImageID       string
+	ImageName     string
+	LabelSelector string
+}
+
+// TFID returns the data source identifier.
+func (d *DData) TFID() string {
+	return fmt.Sprintf("data.%s.%s", DataSourceType, d.RName())
+}
