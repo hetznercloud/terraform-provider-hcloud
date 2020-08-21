a9b52b45aa283055269070c5d94afcbec9ea164e
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-08-21 10:30:00 +0200

    Refactor hcloud_server tests

--- hcloud/provider_test.go
+++ hcloud/provider_test.go
@@ -1,23 +1,11 @@
 package hcloud
 
 import (
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
-
-// Deprecated: use testsupport.AccTestProviders instead
-var testAccProviders map[string]terraform.ResourceProvider
-var testAccProvider *schema.Provider
-
-func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
-		"hcloud": testAccProvider,
-	}
-}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
@@ -27,11 +15,4 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ terraform.ResourceProvider = Provider()
-}
-
-// Deprecated: use testsupport.AcceptanceTestPreCheck instead
-func testAccHcloudPreCheck(t *testing.T) {
-	if v := os.Getenv("HCLOUD_TOKEN"); v == "" {
-		t.Fatal("HCLOUD_TOKEN must be set for acceptance tests")
-	}
 }
