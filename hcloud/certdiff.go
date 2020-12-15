3fde7bd7dffe5647fa8805c557bd92f07396f333
Ferdinand Hofherr <ferdinand.hofherr@hetzner-cloud.de>
2020-12-15 07:06:02 +0100

    Compare certificates by their content (#282)
    This provides a fix for the issue observed in GH-271. Certificates
    are now compared by their parsed content instead of their string
    representation.

---
+++ hcloud/certdiff.go
@@ -0,0 +1,73 @@
+package hcloud
+
+import (
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+)
+
+// EqualCert compares two PEM encoded X509 certificates.
+//
+// EqualCert returns an error if it was unable to parse any of the passed
+// certificates.
+func EqualCert(cert1, cert2 string) (bool, error) {
+	const op = "certificate/EqualCert"
+
+	if cert1 == "" || cert2 == "" {
+		return false, nil
+	}
+
+	parsed1, err := parseCertificates(cert1)
+	if err != nil {
+		return false, fmt.Errorf("%s: cert1: %v", op, err)
+	}
+	parsed2, err := parseCertificates(cert2)
+	if err != nil {
+		return false, fmt.Errorf("%s: cert2: %v", op, err)
+	}
+	if len(parsed1) != len(parsed2) {
+		return false, nil
+	}
+
+	hashes1 := calcCertHashes(parsed1)
+	hashes2 := calcCertHashes(parsed2)
+	for h := range hashes1 {
+		if !hashes2[h] {
+			return false, nil
+		}
+		delete(hashes1, h)
+		delete(hashes2, h)
+	}
+
+	return len(hashes1) == 0 && len(hashes2) == 0, nil
+}
+
+func calcCertHashes(cs []*x509.Certificate) map[[sha256.Size]byte]bool {
+	hs := make(map[[sha256.Size]byte]bool, len(cs))
+	for _, c := range cs {
+		h := sha256.Sum256(c.Raw)
+		hs[h] = true
+	}
+	return hs
+}
+
+func parseCertificates(cert string) ([]*x509.Certificate, error) {
+	const op = "certificate/parseCertificate"
+	var certs []*x509.Certificate
+
+	rest := []byte(cert)
+	for len(rest) > 0 {
+		var blk *pem.Block
+
+		blk, rest = pem.Decode(rest)
+		c, err := x509.ParseCertificate(blk.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", op, err)
+		}
+
+		certs = append(certs, c)
+	}
+
+	return certs, nil
+}
---
+++ internal/certificate/certdiff_test.go
@@ -0,0 +1,106 @@
+package certificate_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/hetznercloud/terraform-provider-hcloud/hcloud"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/testsupport"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEqualCert_EqualCertificates(t *testing.T) {
+	tests := []struct {
+		name string
+		cns  []string
+	}{
+		{
+			name: "One certificate",
+			cns:  []string{"aaa"},
+		},
+		{
+			name: "Multiple certificates",
+			cns:  []string{"aaa", "bbb"},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			certs := make([]string, len(tt.cns))
+			for i, cn := range tt.cns {
+				cert, _, err := testsupport.RandTLSCert(cn)
+				if !assert.NoError(t, err) {
+					return
+				}
+				certs[i] = cert
+			}
+
+			cert := strings.Join(certs, "\n")
+			res, err := hcloud.EqualCert(cert, cert)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			assert.True(t, res, "Same certificates are expected to be equal")
+		})
+	}
+}
+
+func TestEqualCert_DifferentCertificates(t *testing.T) {
+	tests := []struct {
+		name   string
+		cns    []string
+		cnsAlt []string
+	}{
+		{
+			name:   "one certificate - same cn",
+			cns:    []string{"aaa"},
+			cnsAlt: []string{"aaa"},
+		},
+		{
+			name:   "multiple certificates - same cns",
+			cns:    []string{"aaa", "bbb"},
+			cnsAlt: []string{"aaa", "bbb"},
+		},
+		{
+			name:   "first empty",
+			cnsAlt: []string{"aaa", "bbb"},
+		},
+		{
+			name: "second empty",
+			cns:  []string{"aaa", "bbb"},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			certs := make([]string, len(tt.cns))
+			for i, cn := range tt.cns {
+				cert, _, err := testsupport.RandTLSCert(cn)
+				if !assert.NoError(t, err) {
+					return
+				}
+				certs[i] = cert
+			}
+
+			certsAlt := make([]string, len(tt.cnsAlt))
+			for i, cn := range tt.cnsAlt {
+				cert, _, err := testsupport.RandTLSCert(cn)
+				if !assert.NoError(t, err) {
+					return
+				}
+				certsAlt[i] = cert
+			}
+
+			cert := strings.Join(certs, "\n")
+			certAlt := strings.Join(certsAlt, "\n")
+			res, err := hcloud.EqualCert(cert, certAlt)
+			if !assert.NoError(t, err) {
+				return
+			}
+			assert.False(t, res, "Different certificates are expected to be unequal")
+		})
+	}
+}
