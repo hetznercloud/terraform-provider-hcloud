18222e47289c0f5dcb83cc736da0a2b551ac5321
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2021-01-15 15:11:38 +0100

    Improve testing of hcloud provider
    Make sure that we register always all wanted resources/datasources

--- hcloud/provider_test.go
+++ hcloud/provider_test.go
@@ -1,9 +1,20 @@
 package hcloud
 
 import (
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/certificate"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/datacenter"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/floatingip"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/image"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/loadbalancer"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/location"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/network"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/rdns"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/server"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/servertype"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/sshkey"
+	"github.com/hetznercloud/terraform-provider-hcloud/internal/volume"
+	"github.com/stretchr/testify/assert"
 	"testing"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func TestProvider(t *testing.T) {
@@ -12,6 +23,59 @@ func TestProvider(t *testing.T) {
 	}
 }
 
-func TestProvider_impl(t *testing.T) {
-	var _ *schema.Provider = Provider()
+func TestProvider_Resources(t *testing.T) {
+	var provider = Provider()
+	expectedResources := []string{
+		certificate.ResourceType,
+		floatingip.AssignmentResourceType,
+		floatingip.ResourceType,
+		loadbalancer.NetworkResourceType,
+		loadbalancer.ResourceType,
+		loadbalancer.ServiceResourceType,
+		loadbalancer.TargetResourceType,
+		network.ResourceType,
+		network.RouteResourceType,
+		network.SubnetResourceType,
+		rdns.ResourceType,
+		server.NetworkResourceType,
+		server.ResourceType,
+		sshkey.ResourceType,
+		volume.AttachmentResourceType,
+		volume.ResourceType,
+	}
+
+	resources := provider.Resources()
+	assert.Len(t, resources, len(expectedResources))
+
+	for _, datasource := range resources {
+		assert.Contains(t, expectedResources, datasource.Name)
+	}
+}
+
+func TestProvider_DataSources(t *testing.T) {
+	var provider = Provider()
+	expectedDataSources := []string{
+		certificate.DataSourceType,
+		datacenter.DatacentersDataSourceType,
+		datacenter.DataSourceType,
+		floatingip.DataSourceType,
+		image.DataSourceType,
+		loadbalancer.DataSourceType,
+		location.DataSourceType,
+		location.LocationsDataSourceType,
+		network.DataSourceType,
+		server.DataSourceType,
+		servertype.DataSourceType,
+		servertype.ServerTypesDataSourceType,
+		sshkey.DataSourceType,
+		sshkey.SSHKeysDataSourceType,
+		volume.DataSourceType,
+	}
+
+	datasources := provider.DataSources()
+	assert.Len(t, datasources, len(expectedDataSources))
+
+	for _, datasource := range datasources {
+		assert.Contains(t, expectedDataSources, datasource.Name)
+	}
 }
