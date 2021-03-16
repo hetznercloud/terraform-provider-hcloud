61f51a17a53967d992a8fcb7c92bea363c66afcb
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2021-03-16 07:01:35 +0100

    `hcloud_firewall` resource: plugin normalized CIDRs silently
    The terraform provider normalized the given CIDRs by default, so when a user entered 10.0.0.1/8 (as a sample) the terraform provider normalized it to 10.0.0.0/8. This lead to a state difference after every apply, so that terraform tries to recreate the firewall rules. After this MR we now validate that the given IP is the start of the CIDR block (e.g. 10.0.0.0/8).
    
    Signed-off-by: Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>

---
+++ internal/firewall/validation.go
@@ -0,0 +1,20 @@
+package firewall
+
+import (
+	"net"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+)
+
+func validateIPDiag(i interface{}, path cty.Path) diag.Diagnostics {
+	ipS := i.(string)
+	ip, n, err := net.ParseCIDR(ipS)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if ip.String() != n.IP.String() {
+		return diag.Errorf("%s is not the start of the cidr block %s", ipS, n)
+	}
+	return nil
+}
