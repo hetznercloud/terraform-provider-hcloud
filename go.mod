5e97a5c7447adf63ff054794a59c2efd19e7c1c2
Adrian Huber <adrian.huber@hetzner-cloud.de>
2021-08-19 10:32:18 +0200

    Add dns ptr support for load balancer

--- go.mod
+++ go.mod
@@ -4,72 +4,67 @@ require (
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
-	github.com/hetznercloud/hcloud-go v1.30.0
+	github.com/hetznercloud/hcloud-go v1.31.1
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
-	golang.org/x/net v0.0.0-20210326060303-6b1517762897
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
 )
 
 require (
-	cloud.google.com/go v0.61.0 // indirect
-	cloud.google.com/go/storage v1.10.0 // indirect
-	github.com/agext/levenshtein v1.2.2 // indirect
-	github.com/apparentlymart/go-cidr v1.0.1 // indirect
-	github.com/apparentlymart/go-textseg v1.0.0 // indirect
+	cloud.google.com/go v0.93.3 // indirect
+	cloud.google.com/go/storage v1.16.0 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-cidr v1.1.0 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
-	github.com/aws/aws-sdk-go v1.25.3 // indirect
+	github.com/aws/aws-sdk-go v1.40.25 // indirect
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.7.0 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-getter v1.5.3 // indirect
-	github.com/hashicorp/go-hclog v0.15.0 // indirect
-	github.com/hashicorp/go-plugin v1.4.1 // indirect
+	github.com/hashicorp/go-getter v1.5.7 // indirect
+	github.com/hashicorp/go-hclog v0.16.2 // indirect
+	github.com/hashicorp/go-plugin v1.4.2 // indirect
 	github.com/hashicorp/go-safetemp v1.0.0 // indirect
-	github.com/hashicorp/go-uuid v1.0.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/hashicorp/go-version v1.3.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.3.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.10.1 // indirect
 	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/terraform-exec v0.14.0 // indirect
 	github.com/hashicorp/terraform-json v0.12.0 // indirect
-	github.com/hashicorp/terraform-plugin-go v0.3.0 // indirect
-	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
-	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
-	github.com/jstemmer/go-junit-report v0.9.1 // indirect
-	github.com/klauspost/compress v1.11.2 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/hashicorp/terraform-plugin-go v0.3.1 // indirect
+	github.com/hashicorp/yamux v0.0.0-20210707203944-259a57b3608c // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/go-testing-interface v1.0.4 // indirect
-	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/oklog/run v1.0.0 // indirect
+	github.com/oklog/run v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.1 // indirect
-	github.com/ulikunitz/xz v0.5.8 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	github.com/zclconf/go-cty v1.8.4 // indirect
-	go.opencensus.io v0.22.4 // indirect
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/mod v0.3.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	golang.org/x/tools v0.0.0-20200713011307-fd294ab11aed // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	google.golang.org/api v0.29.0 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/genproto v0.0.0-20200711021454-869866162049 // indirect
-	google.golang.org/grpc v1.32.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	github.com/zclconf/go-cty v1.9.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210817223510-7df4dd6e12ab // indirect
+	golang.org/x/sys v0.0.0-20210819072135-bce67f096156 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.54.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210818220304-27ea9cc85d9f // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 go 1.17
--- internal/rdns/resource_hcloud_rdns.go
+++ internal/rdns/resource_hcloud_rdns.go
@@ -2,9 +2,7 @@ package rdns
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"log"
 	"net"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -32,14 +30,22 @@ func Resource() *schema.Resource {
 		},
 		Schema: map[string]*schema.Schema{
 			"server_id": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				ForceNew: true,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     true,
+				ExactlyOneOf: []string{"server_id", "floating_ip_id", "load_balancer_id"},
 			},
 			"floating_ip_id": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				ForceNew: true,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     true,
+				ExactlyOneOf: []string{"server_id", "floating_ip_id", "load_balancer_id"},
+			},
+			"load_balancer_id": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     true,
+				ExactlyOneOf: []string{"server_id", "floating_ip_id", "load_balancer_id"},
 			},
 			"ip_address": {
 				Type:     schema.TypeString,
@@ -55,161 +61,101 @@ func Resource() *schema.Resource {
 }
 
 func resourceReverseDNSRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	client := m.(*hcloud.Client)
+	c := m.(*hcloud.Client)
 
-	server, floatingIP, ip, err := lookupRDNSID(ctx, d.Id(), client)
-	if err == errInvalidRDNSID {
-		log.Printf("[WARN] Invalid id (%s), removing from state: %s", d.Id(), err)
-		d.SetId("")
-		return nil
-	}
+	rdns, ip, err := lookupRDNSID(ctx, d.Id(), c)
 	if err != nil {
+		d.SetId("")
 		return hcclient.ErrorToDiag(err)
 	}
-	if server == nil && floatingIP == nil {
-		log.Printf("[WARN] RDNS entry (%s) not found, removing from state", d.Id())
+	dns, err := rdns.GetDNSPtrForIP(ip)
+	if err != nil {
 		d.SetId("")
-		return nil
+		return hcclient.ErrorToDiag(err)
 	}
 
-	if floatingIP != nil {
-		dnsPtr := floatingIP.DNSPtrForIP(ip)
-		if dnsPtr == "" {
-			log.Printf("[WARN] RDNS entry (%s) not found, removing from state", d.Id())
-			d.SetId("")
-			return nil
-		}
+	d.SetId(generateRDNSID(rdns, ip))
+	d.Set("dns_ptr", dns)
+	d.Set("ip_address", ip.String())
 
-		d.Set("dns_ptr", dnsPtr)
-		d.Set("floating_ip_id", floatingIP.ID)
-		d.Set("ip_address", ip.String())
-		d.SetId(generateRDNSID(nil, floatingIP, ip.String()))
-		return nil
+	switch v := rdns.(type) {
+	case *hcloud.Server:
+		d.Set("server_id", v.ID)
+	case *hcloud.FloatingIP:
+		d.Set("floating_ip_id", v.ID)
+	case *hcloud.LoadBalancer:
+		d.Set("load_balancer_id", v.ID)
+	default:
+		d.SetId("")
+		return diag.Errorf("RDNS does not support %+v", v)
 	}
 
-	if ip.To4() != nil {
-		d.SetId(generateRDNSID(server, nil, ip.String()))
-		d.Set("dns_ptr", server.PublicNet.IPv4.DNSPtr)
-		d.Set("server_id", server.ID)
-		d.Set("ip_address", server.PublicNet.IPv4.IP.String())
-		return nil
-	}
-
-	for rdns := range server.PublicNet.IPv6.DNSPtr {
-		if rdns == ip.String() {
-			d.SetId(generateRDNSID(server, nil, ip.String()))
-			d.Set("dns_ptr", server.PublicNet.IPv6.DNSPtrForIP(ip))
-			d.Set("ip_address", ip.String())
-			d.Set("server_id", server.ID)
-			return nil
-		}
-	}
-
-	log.Printf("[WARN] RDNS entry (%s) not found, removing from state", d.Id())
-	d.SetId("")
 	return nil
 }
 
 func resourceReverseDNSCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hcloud.Client)
-	ip := d.Get("ip_address").(string)
-	ptr := d.Get("dns_ptr").(string)
-
 	serverID, serverOK := d.GetOk("server_id")
 	floatingIPID, floatingIPOK := d.GetOk("floating_ip_id")
-	if serverOK && floatingIPOK {
-		log.Printf("[ERR] server_id and floating_ip_id are mutually exclusive, resource %s", d.Id())
-		d.SetId("")
-		return nil
-	}
-	if serverOK {
-		server, _, err := c.Server.GetByID(ctx, serverID.(int))
-		if err != nil {
-			return hcclient.ErrorToDiag(err)
-		}
-		if server == nil {
-			log.Printf("[WARN] Server (%s) not found, removing from state", d.Id())
-			d.SetId("")
-			return nil
-		}
+	loadBalancerID, loadBalancerOK := d.GetOk("load_balancer_id")
 
-		d.SetId(generateRDNSID(server, nil, ip))
-		action, _, err := c.Server.ChangeDNSPtr(ctx, server, ip, &ptr)
-		if err != nil {
-			return hcclient.ErrorToDiag(err)
-		}
-		if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
-			return hcclient.ErrorToDiag(err)
-		}
-
-		return resourceReverseDNSRead(ctx, d, m)
+	ip := net.ParseIP(d.Get("ip_address").(string))
+	if ip == nil {
+		return hcclient.ErrorToDiag(fmt.Errorf("could not parse ip %s", d.Get("ip_address").(string)))
 	}
-	if !floatingIPOK {
-		log.Printf("[WARN] Invalid floating_ip_id (%s), removing from state: %v", d.Id(), floatingIPOK)
-		d.SetId("")
-		return nil
+	ptr := d.Get("dns_ptr").(string)
+
+	var rdns hcloud.RDNSSupporter
+	var err error
+
+	switch {
+	case serverOK:
+		rdns, _, err = c.Server.GetByID(ctx, serverID.(int))
+	case floatingIPOK:
+		rdns, _, err = c.FloatingIP.GetByID(ctx, floatingIPID.(int))
+	case loadBalancerOK:
+		rdns, _, err = c.LoadBalancer.GetByID(ctx, loadBalancerID.(int))
 	}
 
-	floatingIP, _, err := c.FloatingIP.GetByID(ctx, floatingIPID.(int))
-	if err != nil {
-		return hcclient.ErrorToDiag(err)
-	}
-	if floatingIP == nil {
-		log.Printf("[WARN] Floating IP (%s) not found, removing from state", d.Id())
-		d.SetId("")
-		return nil
-	}
-
-	d.SetId(generateRDNSID(nil, floatingIP, ip))
-	action, _, err := c.FloatingIP.ChangeDNSPtr(ctx, floatingIP, ip, &ptr)
 	if err != nil {
 		return hcclient.ErrorToDiag(err)
 	}
 
+	d.SetId(generateRDNSID(rdns, ip))
+	action, _, err := c.RDNS.ChangeDNSPtr(ctx, rdns, ip, &ptr)
+	if err != nil {
+		return hcclient.ErrorToDiag(err)
+	}
 	if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
 		return hcclient.ErrorToDiag(err)
 	}
+
 	return resourceReverseDNSRead(ctx, d, m)
 }
 
 func resourceReverseDNSUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hcloud.Client)
 
-	server, floatingIP, _, err := lookupRDNSID(ctx, d.Id(), c)
-	if err == errInvalidRDNSID {
-		log.Printf("[WARN] Invalid id (%s), removing from state: %s", d.Id(), err)
-		d.SetId("")
-		return nil
-	}
+	rdns, _, err := lookupRDNSID(ctx, d.Id(), c)
 	if err != nil {
+		d.SetId("")
 		return hcclient.ErrorToDiag(err)
 	}
-	if server == nil && floatingIP == nil {
-		log.Printf("[WARN] RDNS entry (%s) not found, removing from state", d.Id())
-		d.SetId("")
-		return nil
-	}
 
-	ip := d.Get("ip_address").(string)
+	ip := net.ParseIP(d.Get("ip_address").(string))
+	if ip == nil {
+		return hcclient.ErrorToDiag(fmt.Errorf("could not parse ip %s", d.Get("ip_address").(string)))
+	}
 	ptr := d.Get("dns_ptr").(string)
 
 	if d.HasChange("dns_ptr") {
-		if floatingIP != nil {
-			action, _, err := c.FloatingIP.ChangeDNSPtr(ctx, floatingIP, ip, &ptr)
-			if err != nil {
-				return hcclient.ErrorToDiag(err)
-			}
-			if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
-				return hcclient.ErrorToDiag(err)
-			}
-		} else if server != nil {
-			action, _, err := c.Server.ChangeDNSPtr(ctx, server, ip, &ptr)
-			if err != nil {
-				return hcclient.ErrorToDiag(err)
-			}
-			if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
-				return hcclient.ErrorToDiag(err)
-			}
+		action, _, err := c.RDNS.ChangeDNSPtr(ctx, rdns, ip, &ptr)
+		if err != nil {
+			return hcclient.ErrorToDiag(err)
+		}
+
+		if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
+			return hcclient.ErrorToDiag(err)
 		}
 	}
 	return resourceReverseDNSRead(ctx, d, m)
@@ -218,44 +164,22 @@ func resourceReverseDNSUpdate(ctx context.Context, d *schema.ResourceData, m int
 func resourceReverseDNSDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hcloud.Client)
 
-	server, floatingIP, ip, err := lookupRDNSID(ctx, d.Id(), c)
-	if err == errInvalidRDNSID {
-		log.Printf("[WARN] Invalid id (%s), removing from state: %s", d.Id(), err)
-		d.SetId("")
-		return nil
-	}
+	rdns, ip, err := lookupRDNSID(ctx, d.Id(), c)
 	if err != nil {
+		d.SetId("")
 		return hcclient.ErrorToDiag(err)
 	}
-	if server == nil && floatingIP == nil {
-		log.Printf("[WARN] RDNS entry (%s) not found, removing from state", d.Id())
-		d.SetId("")
-		return nil
-	}
 
-	if floatingIP != nil {
-		action, _, err := c.FloatingIP.ChangeDNSPtr(ctx, floatingIP, ip.String(), nil)
-		if err != nil {
-			if hcerr, ok := err.(hcloud.Error); ok && hcerr.Code == hcloud.ErrorCodeNotFound {
-				// floating ip has already been deleted
-				return nil
-			}
-			return hcclient.ErrorToDiag(err)
-		}
-		if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
-			return hcclient.ErrorToDiag(err)
-		}
-		return nil
-	}
+	action, _, err := c.RDNS.ChangeDNSPtr(ctx, rdns, ip, nil)
 
-	action, _, err := c.Server.ChangeDNSPtr(ctx, server, ip.String(), nil)
 	if err != nil {
 		if hcerr, ok := err.(hcloud.Error); ok && hcerr.Code == hcloud.ErrorCodeNotFound {
-			// server has already been deleted
+			// resource has already been deleted
 			return nil
 		}
 		return hcclient.ErrorToDiag(err)
 	}
+
 	if err := hcclient.WaitForAction(ctx, &c.Action, action); err != nil {
 		return hcclient.ErrorToDiag(err)
 	}
@@ -263,17 +187,26 @@ func resourceReverseDNSDelete(ctx context.Context, d *schema.ResourceData, m int
 	return nil
 }
 
-func generateRDNSID(server *hcloud.Server, floatingIP *hcloud.FloatingIP, ip string) string {
-	if server != nil {
-		return fmt.Sprintf("s-%d-%s", server.ID, ip)
+func generateRDNSID(rdns hcloud.RDNSSupporter, ip net.IP) string {
+	switch v := rdns.(type) {
+	case *hcloud.Server:
+		return fmt.Sprintf("s-%d-%s", v.ID, ip)
+	case *hcloud.FloatingIP:
+		return fmt.Sprintf("f-%d-%s", v.ID, ip)
+	case *hcloud.LoadBalancer:
+		return fmt.Sprintf("l-%d-%s", v.ID, ip)
+	default:
+		return ""
 	}
-	if floatingIP != nil {
-		return fmt.Sprintf("f-%d-%s", floatingIP.ID, ip)
-	}
-	return ""
 }
 
-var errInvalidRDNSID = errors.New("invalid rdns id")
+type InvalidRDNSIDError struct {
+	ID string
+}
+
+func (e InvalidRDNSIDError) Error() string {
+	return fmt.Sprintf("invalid rdns id %s", e.ID)
+}
 
 // lookupRDNSID parses the terraform rdns record id and returns the associated server or floating ip
 //
@@ -281,38 +214,39 @@ var errInvalidRDNSID = errors.New("invalid rdns id")
 // Examples:
 // s-123-192.168.100.1 (reverse dns entry on server with id 123, ip 192.168.100.1)
 // f-123-2001:db8::1 (reverse dns entry on floating ip with id 123, ip 2001:db8::1)
-func lookupRDNSID(ctx context.Context, terraformID string, client *hcloud.Client) (
-	server *hcloud.Server, floatingIP *hcloud.FloatingIP, ip net.IP, err error) {
+// l-123-2001:db8::1 (reverse dns entry on load balancer with id 123, ip 2001:db8::1)
+func lookupRDNSID(ctx context.Context, terraformID string, client *hcloud.Client) (hcloud.RDNSSupporter, net.IP, error) {
 	if terraformID == "" {
-		err = errInvalidRDNSID
-		return
+		return nil, nil, InvalidRDNSIDError{terraformID}
 	}
 
 	parts := strings.SplitN(terraformID, "-", 3)
 	if len(parts) != 3 {
-		err = errInvalidRDNSID
-		return
+		return nil, nil, InvalidRDNSIDError{terraformID}
 	}
 
 	id, err := strconv.Atoi(parts[1])
 	if err != nil {
-		err = errInvalidRDNSID
-		return
+		return nil, nil, InvalidRDNSIDError{terraformID}
 	}
 
-	ip = net.ParseIP(parts[2])
+	ip := net.ParseIP(parts[2])
 	if ip == nil {
-		err = errInvalidRDNSID
-		return
+		return nil, nil, InvalidRDNSIDError{terraformID}
 	}
+
+	var rdns hcloud.RDNSSupporter
 
 	switch parts[0] {
 	case "s":
-		server, _, err = client.Server.GetByID(ctx, id)
+		rdns, _, err = client.Server.GetByID(ctx, id)
 	case "f":
-		floatingIP, _, err = client.FloatingIP.GetByID(ctx, id)
+		rdns, _, err = client.FloatingIP.GetByID(ctx, id)
+	case "l":
+		rdns, _, err = client.LoadBalancer.GetByID(ctx, id)
 	default:
-		err = errInvalidRDNSID
+		err = InvalidRDNSIDError{terraformID}
 	}
-	return
+
+	return rdns, ip, err
 }
