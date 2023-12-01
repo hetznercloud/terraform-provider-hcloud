225423c62141fa159500856fa7cced51cfab1733
David Morcillo Muñoz <david.morcillo@factorial.co>
2023-12-01 09:18:04 +0100

    Add missing import

--- hcloud/provider.go
+++ hcloud/provider.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hetznercloud/hcloud-go/hcloud"
