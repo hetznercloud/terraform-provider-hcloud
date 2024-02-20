40df28d79dd192ef39edb51dfd6c4a6626d6413b
Julian Tölle <julian.toelle@hetzner-cloud.de>
2024-02-20 08:19:59 +0100

    feat(firewall): set IPs without prefix directly (#874)
    This commit allows users to specify allowed IPs directly, without having
    to awkwardly put an `/32` at the end.
    
    If an IP is passed, we only allow this single IP (`/32` for IPv4, `/128`
    for IPv6). This matches the behavior in Cloud Console.
    
    Closes #807
    Closes #715
    
    fix(firewall): unnecessary diff if user specified non-minimal IPv6
      If the user specified an IPv6 which was not minimal (eg. `::0` instead
      of `::`), the API would modify this to the minimal form. In the next run
      terraform reports a diff and tries to update this over and over again.
      We now do this normalization locally to avoid showing these diffs to
      users.
    
      Closes #870
    
    
    Co-authored-by: Ettore Foti <ettorefoti@gmail.com>

--- internal/firewall/validation.go
+++ internal/firewall/validation.go
@@ -5,10 +5,19 @@ import (
 
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
 	"github.com/hetznercloud/terraform-provider-hcloud/internal/util/hcloudutil"
 )
 
+var (
+	// These masks are also used in the Cloud Console when a user enters an IP without range.
+	defaultMaskIPv4 = net.CIDRMask(32, 32)
+	defaultMaskIPv6 = net.CIDRMask(128, 128)
+)
+
 func validateIPDiag(i interface{}, _ cty.Path) diag.Diagnostics {
+	i = normalizeIP(i)
+
 	ipS := i.(string)
 	ip, n, err := net.ParseCIDR(ipS)
 	if err != nil {
@@ -18,4 +27,42 @@ func validateIPDiag(i interface{}, _ cty.Path) diag.Diagnostics {
 		return diag.Errorf("%s is not the start of the cidr block %s", ipS, n)
 	}
 	return nil
+}
+
+// normalizeIP implements two closely related functions:
+//  1. It normalizes an IP address or CIDR block to a CIDR block. To allow users to specify the IP directly.
+//  2. The API modifies CIDRs to lower case and IPv6 to its minimal form. This function does the same to
+//     have clean diffs, even if the user input does not match the desired format by the API.
+func normalizeIP(i interface{}) string {
+	input := i.(string)
+
+	ip, ipnet, err := net.ParseCIDR(input)
+	if err == nil {
+		// net.ParseCIDR removes any set host bits. We want to show an error to the user instead,
+		// to avoid making any assumptions about their intent.
+		// By setting the parse IP in the ipnet, the returned string will be the same as the input, only normalized & lower cased.
+		ipnet.IP = ip
+	} else {
+		ip = net.ParseIP(input)
+		if ip == nil {
+			// No CIDR or IP, just return the input string
+			return input
+		}
+		if ip.To4() != nil {
+			// IPv4
+
+			ipnet = &net.IPNet{
+				IP:   ip,
+				Mask: defaultMaskIPv4,
+			}
+		} else {
+			// If To4 returns nil, IP is not IPv4 => IPv6
+			ipnet = &net.IPNet{
+				IP:   ip,
+				Mask: defaultMaskIPv6,
+			}
+		}
+	}
+
+	return ipnet.String()
 }
