e8fab046d71551a970624b3515c10ee850315221
Jonas L. <jooola@users.noreply.github.com>
2024-09-06 13:26:36 +0200

    refactor: add deprecation mixin for plugin framework (#982)
    Add a deprecation mixin to be used by resource/datasource using the
    plugin framework.

---
+++ internal/deprecation/deprecation_plugin.go
@@ -0,0 +1,79 @@
+package deprecation
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	datasourceschema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	resourceschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hetznercloud/hcloud-go/hcloud"
+)
+
+type DeprecationModel struct { // nolint:revive
+	IsDeprecated         types.Bool   `tfsdk:"is_deprecated"`
+	DeprecationAnnounced types.String `tfsdk:"deprecation_announced"`
+	UnavailableAfter     types.String `tfsdk:"unavailable_after"`
+}
+
+func NewDeprecationModel(_ context.Context, in hcloud.Deprecatable) (DeprecationModel, diag.Diagnostics) {
+	var data DeprecationModel
+	var diags diag.Diagnostics
+
+	if in.IsDeprecated() {
+		data.IsDeprecated = types.BoolValue(true)
+		data.DeprecationAnnounced = types.StringValue(in.DeprecationAnnounced().Format(time.RFC3339))
+		data.UnavailableAfter = types.StringValue(in.UnavailableAfter().Format(time.RFC3339))
+	} else {
+		data.IsDeprecated = types.BoolValue(false)
+
+		// TODO: Stored values should be types.StringNull(), but we use an empty string
+		// for backward compatibility with the SDK.
+		data.DeprecationAnnounced = types.StringValue("")
+		data.UnavailableAfter = types.StringValue("")
+	}
+
+	return data, diags
+}
+
+func AttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"is_deprecated":         types.BoolType,
+		"deprecation_announced": types.StringType,
+		"unavailable_after":     types.StringType,
+	}
+}
+
+func DataSourceSchema() map[string]datasourceschema.Attribute {
+	return map[string]datasourceschema.Attribute{
+		"is_deprecated": datasourceschema.BoolAttribute{
+			Computed: true,
+		},
+		"deprecation_announced": datasourceschema.StringAttribute{
+			Computed: true,
+			Optional: true,
+		},
+		"unavailable_after": datasourceschema.StringAttribute{
+			Computed: true,
+			Optional: true,
+		},
+	}
+}
+
+func ResourceSchema() map[string]resourceschema.Attribute {
+	return map[string]resourceschema.Attribute{
+		"is_deprecated": resourceschema.BoolAttribute{
+			Computed: true,
+		},
+		"deprecation_announced": resourceschema.StringAttribute{
+			Computed: true,
+			Optional: true,
+		},
+		"unavailable_after": resourceschema.StringAttribute{
+			Computed: true,
+			Optional: true,
+		},
+	}
+}
---
+++ internal/deprecation/deprecation_plugin_test.go
@@ -0,0 +1,40 @@
+package deprecation
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/hetznercloud/hcloud-go/hcloud"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDeprecationModel(t *testing.T) {
+	ctx := context.Background()
+	fakeTime := time.Date(2024, time.September, 6, 12, 0, 0, 0, time.UTC)
+
+	{
+		data, diags := NewDeprecationModel(ctx, hcloud.ServerType{})
+		assert.Equal(t, diags.HasError(), false)
+		assert.Equal(t, data.IsDeprecated.ValueBool(), false)
+		assert.Equal(t, data.DeprecationAnnounced.IsNull(), false)
+		assert.Equal(t, data.DeprecationAnnounced.ValueString(), "")
+		assert.Equal(t, data.UnavailableAfter.IsNull(), false)
+		assert.Equal(t, data.UnavailableAfter.ValueString(), "")
+	}
+
+	{
+		data, diags := NewDeprecationModel(ctx, hcloud.ServerType{
+			DeprecatableResource: hcloud.DeprecatableResource{
+				Deprecation: &hcloud.DeprecationInfo{
+					Announced:        fakeTime,
+					UnavailableAfter: fakeTime,
+				},
+			},
+		})
+		assert.Equal(t, diags.HasError(), false)
+		assert.Equal(t, data.IsDeprecated.ValueBool(), true)
+		assert.Equal(t, data.DeprecationAnnounced.ValueString(), "2024-09-06T12:00:00Z")
+		assert.Equal(t, data.UnavailableAfter.ValueString(), "2024-09-06T12:00:00Z")
+	}
+}
