9b234f5b60f919ca25f033726c8bf02598952bba
jo <ljonas@riseup.net>
2024-11-08 18:08:16 +0100

    docs: generate and fix docs for location data sources

--- examples/data-sources/hcloud_locations/data-source.tf
+++ examples/data-sources/hcloud_locations/data-source.tf
@@ -1,11 +1,10 @@
-data "hcloud_locations" "ds" {
-}
+data "hcloud_locations" "all" {}
 
 resource "hcloud_server" "workers" {
   count = 5
 
   name        = "node${count.index}"
-  image       = "debian-11"
+  image       = "debian-12"
   server_type = "cx22"
-  location    = element(data.hcloud_locations.ds.locations, count.index).name
+  location    = element(data.hcloud_locations.all.locations, count.index).name
 }
