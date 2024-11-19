a7624262890760d38e80d6a1491eaf4ddf14ae28
jo <ljonas@riseup.net>
2024-11-19 18:02:01 +0100

    docs: configure autogenerated docs

---
+++ tools/tools.go
@@ -0,0 +1,7 @@
+package tools
+
+import (
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name hcloud
