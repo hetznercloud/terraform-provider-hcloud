8bf73e7914bf4ae38ac1085e5eb161510be1a54d
Jonas L. <jooola@users.noreply.github.com>
2025-09-08 15:23:34 +0200

    feat: add category property to server type (#1184)
    Related to
    https://docs.hetzner.cloud/changelog#2025-08-25-category-for-server-types

--- internal/servertype/data_source_test.go
+++ internal/servertype/data_source_test.go
@@ -32,6 +32,7 @@ func TestAccServerTypeDataSource(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(byName.TFID(), "id", "22"),
 					resource.TestCheckResourceAttr(byName.TFID(), "name", "cpx11"),
+					resource.TestCheckResourceAttr(byName.TFID(), "category", "Shared vCPU"),
 					resource.TestCheckResourceAttr(byName.TFID(), "cores", "2"),
 					resource.TestCheckResourceAttr(byName.TFID(), "memory", "2"),
 					resource.TestCheckResourceAttr(byName.TFID(), "disk", "40"),
@@ -45,6 +46,7 @@ func TestAccServerTypeDataSource(t *testing.T) {
 
 					resource.TestCheckResourceAttr(byID.TFID(), "id", "22"),
 					resource.TestCheckResourceAttr(byID.TFID(), "name", "cpx11"),
+					resource.TestCheckResourceAttr(byID.TFID(), "category", "Shared vCPU"),
 					resource.TestCheckResourceAttr(byID.TFID(), "cores", "2"),
 					resource.TestCheckResourceAttr(byID.TFID(), "memory", "2"),
 					resource.TestCheckResourceAttr(byID.TFID(), "disk", "40"),
@@ -56,49 +58,6 @@ func TestAccServerTypeDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr(byID.TFID(), "deprecation_announced", ""),
 					resource.TestCheckResourceAttr(byID.TFID(), "unavailable_after", ""),
 				),
-			},
-		},
-	})
-}
-
-func TestAccServerTypeDataSource_UpgradePluginFramework(t *testing.T) {
-	tmplMan := testtemplate.Manager{}
-
-	byName := &servertype.DData{ServerTypeName: teste2e.TestServerType}
-	byName.SetRName("by_name")
-
-	byID := &servertype.DData{ServerTypeID: "22"}
-	byID.SetRName("by_id")
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: teste2e.PreCheck(t),
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: map[string]resource.ExternalProvider{
-					"hcloud": {
-						VersionConstraint: "1.48.1",
-						Source:            "hetznercloud/hcloud",
-					},
-				},
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_type", byName,
-					"testdata/d/hcloud_server_type", byID,
-					"testdata/r/terraform_data_resource", byName,
-					"testdata/r/terraform_data_resource", byID,
-				),
-			},
-			{
-				ProtoV6ProviderFactories: teste2e.ProtoV6ProviderFactories(),
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_type", byName,
-					"testdata/d/hcloud_server_type", byID,
-					"testdata/r/terraform_data_resource", byName,
-					"testdata/r/terraform_data_resource", byID,
-				),
-
-				PlanOnly: true,
 			},
 		},
 	})
@@ -140,42 +99,6 @@ func TestAccServerTypeDataSourceList(t *testing.T) {
 					resource.TestCheckResourceAttr(all.TFID(), "server_types.0.deprecation_announced", ""),
 					resource.TestCheckResourceAttr(all.TFID(), "server_types.0.unavailable_after", ""),
 				),
-			},
-		},
-	})
-}
-
-func TestAccServerTypeDataSourceList_UpgradePluginFramework(t *testing.T) {
-	tmplMan := testtemplate.Manager{}
-
-	all := &servertype.DDataList{}
-	all.SetRName("all")
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: teste2e.PreCheck(t),
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: map[string]resource.ExternalProvider{
-					"hcloud": {
-						VersionConstraint: "1.48.1",
-						Source:            "hetznercloud/hcloud",
-					},
-				},
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_types", all,
-					"testdata/r/terraform_data_resource", all,
-				),
-			},
-			{
-				ProtoV6ProviderFactories: teste2e.ProtoV6ProviderFactories(),
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_types", all,
-					"testdata/r/terraform_data_resource", all,
-				),
-
-				PlanOnly: true,
 			},
 		},
 	})
