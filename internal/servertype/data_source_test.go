079fec18c43de49b30d45595b48ec4bc0b500896
jo <ljonas@riseup.net>
2025-09-08 15:10:44 +0200

    test: remove UpgradePluginFramework for server type

--- internal/servertype/data_source_test.go
+++ internal/servertype/data_source_test.go
@@ -63,49 +63,6 @@ func TestAccServerTypeDataSource(t *testing.T) {
 	})
 }
 
-func TestAccServerTypeDataSource_UpgradePluginFramework(t *testing.T) {
-	tmplMan := testtemplate.Manager{}
-
-	byName := &servertype.DData{ServerTypeName: teste2e.TestServerType}
-	byName.SetRName("by_name")
-
-	byID := &servertype.DData{ServerTypeID: "22"}
-	byID.SetRName("by_id")
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: teste2e.PreCheck(t),
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: map[string]resource.ExternalProvider{
-					"hcloud": {
-						VersionConstraint: "1.48.1",
-						Source:            "hetznercloud/hcloud",
-					},
-				},
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_type", byName,
-					"testdata/d/hcloud_server_type", byID,
-					"testdata/r/terraform_data_resource", byName,
-					"testdata/r/terraform_data_resource", byID,
-				),
-			},
-			{
-				ProtoV6ProviderFactories: teste2e.ProtoV6ProviderFactories(),
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_type", byName,
-					"testdata/d/hcloud_server_type", byID,
-					"testdata/r/terraform_data_resource", byName,
-					"testdata/r/terraform_data_resource", byID,
-				),
-
-				PlanOnly: true,
-			},
-		},
-	})
-}
-
 func TestAccServerTypeDataSourceList(t *testing.T) {
 	tmplMan := testtemplate.Manager{}
 
@@ -142,42 +99,6 @@ func TestAccServerTypeDataSourceList(t *testing.T) {
 					resource.TestCheckResourceAttr(all.TFID(), "server_types.0.deprecation_announced", ""),
 					resource.TestCheckResourceAttr(all.TFID(), "server_types.0.unavailable_after", ""),
 				),
-			},
-		},
-	})
-}
-
-func TestAccServerTypeDataSourceList_UpgradePluginFramework(t *testing.T) {
-	tmplMan := testtemplate.Manager{}
-
-	all := &servertype.DDataList{}
-	all.SetRName("all")
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: teste2e.PreCheck(t),
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: map[string]resource.ExternalProvider{
-					"hcloud": {
-						VersionConstraint: "1.48.1",
-						Source:            "hetznercloud/hcloud",
-					},
-				},
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_types", all,
-					"testdata/r/terraform_data_resource", all,
-				),
-			},
-			{
-				ProtoV6ProviderFactories: teste2e.ProtoV6ProviderFactories(),
-
-				Config: tmplMan.Render(t,
-					"testdata/d/hcloud_server_types", all,
-					"testdata/r/terraform_data_resource", all,
-				),
-
-				PlanOnly: true,
 			},
 		},
 	})
