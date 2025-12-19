e07cd120d15217273fc23b90db655180d2dc9d18
Julian Tölle <julian.toelle@hetzner-cloud.de>
2025-12-19 14:47:18 +0100

    feat(storage-box): stop ignoring changes to ssh keys and replace resource instead (#1296)
    The current behaviour is not how most other terraform resources work. In
    general Terraform providers trust that the user reads the proposed plan
    and understands what they mean. We also have no special protection for
    other resources that force them to be re-created, like "user_data" or
    "ssh_keys" on servers.
    
    To aid users I added an example that shows how to set the attribute and
    how to "safeguard" the resource against accidental deletion.
    
    Another reason to remove this behaviour, is that it is not possible to
    import resources right now, as Terraform does accept our empty list in
    face of the users configured SSH Keys as an acceptable state. See #1287
    for this error.
    
    ---
    
    This is a **breaking change** for the `hcloud_storage_box`. This is
    accepted in a minor release as the resource is marked as experimental
    (#1285). I will add a warning to the release notes about this.

--- examples/resources/hcloud_storage_box/resource.tf
+++ examples/resources/hcloud_storage_box/resource.tf
@@ -25,3 +25,29 @@ resource "hcloud_storage_box" "backups" {
 
   delete_protection = true
 }
+
+resource "hcloud_storage_box" "ssh_key" {
+  name             = "backups"
+  storage_box_type = "bx21"
+  location         = "hel1"
+  password         = var.storage_box_password
+
+  # You can set the initial SSH Keys as an attribute on the resource, but these
+  # can not be updated through the API and through the terraform provider.
+  # If this attribute is ever changed, the provider will mark the resource as
+  # "requires replacement" and you could loose the data stored on the Storage Box.
+  ssh_keys = [
+    hcloud_ssh_key.my_key.public_key,
+    file("~/.ssh/id_ed25519.pub"),
+  ]
+
+  lifecycle {
+    # To avoid accidentally deleting the resource you can ignore changes in the ssh_keys attribute:
+    ignore_changes = [
+      ssh_keys
+    ]
+
+    # If you want to be extra sure that your Storage Box is not accidentally deleted:
+    prevent_destroy = true
+  }
+}
