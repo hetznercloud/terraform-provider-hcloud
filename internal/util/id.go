59a95c3c127a2905964b8089f2f764149dd23cc3
phm07 <22707808+phm07@users.noreply.github.com>
2026-05-29 11:56:15 +0200

    fix int32 cast

--- internal/util/id.go
+++ internal/util/id.go
@@ -17,6 +17,29 @@ func CastInt64(v any) int64 {
 	}
 }
 
+func CastInt32(v any) int32 {
+	switch o := v.(type) {
+	case int64:
+		if o > math.MaxInt32 || o < math.MinInt32 {
+			panic(fmt.Sprintf("integer value %d out of bounds for int32", o))
+		}
+		return int32(o)
+	case int:
+		if o > math.MaxInt32 || o < math.MinInt32 {
+			panic(fmt.Sprintf("integer value %d out of bounds for int32", o))
+		}
+		return int32(o)
+	case int32:
+		return o
+	case float32:
+		return CastInt32(int64(o))
+	case float64:
+		return CastInt32(int64(o))
+	default:
+		panic(fmt.Sprintf("unexpected type %T for %#v", v, v))
+	}
+}
+
 func CastInt(v any) int {
 	switch o := v.(type) {
 	case int:
