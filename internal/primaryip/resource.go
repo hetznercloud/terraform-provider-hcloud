31b0fbc0f92dac0e7f23666f2909d33ec0e9f33e
jo <ljonas@riseup.net>
2026-05-29 14:51:10 +0200

    fix: auto_delete default to false

--- internal/primaryip/resource.go
+++ internal/primaryip/resource.go
@@ -126,6 +126,7 @@ communicating with the API.
 			MarkdownDescription: "Whether auto delete is enabled. Setting `auto_delete` to `false` is recommended, because if a server assigned to the managed ip is getting deleted, it will also delete the primary IP which will break the terraform state.",
 			Optional:            true,
 			Computed:            true,
+			Default:             booldefault.StaticBool(false),
 		},
 		"labels": resourceutil.LabelsSchema(),
 		"delete_protection": schema.BoolAttribute{
